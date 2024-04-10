bce12889f93ef30f770993e7dcadc2b93f6036d5
theanuragshukla <tachyonanurag@gmail.com>
2024-04-11 01:07:55 +0530

    🔥 fixed config

--- constants.js
+++ constants.js
@@ -1,8 +1,5 @@
-import fs from 'fs'
-
-const config = JSON.parse(fs.readFileSync('config.json', 'utf8'));
 export const AUTHTOKEN = 'x-auth-token'
 export const PROJECT_NAME = 'envoix'
 export const CONFIG_FILE = 'env-config.json'
-export const SERVER_URL = config.SERVER_URL
+export const SERVER_URL = 'https://envoix.anurags.tech'
 export const ORIGIN = 'envoix-cli'
