275e060f95fe93201069b6bc60ad04c093f3241b
theanuragshukla <tachyonanurag@gmail.com>
2024-04-11 00:52:58 +0530

    🔥 fixed warnings

--- constants.js
+++ constants.js
@@ -1,5 +1,6 @@
-import config from "./config.json" assert { type: "json" };
+import fs from 'fs'
 
+const config = JSON.parse(fs.readFileSync('config.json', 'utf8'));
 export const AUTHTOKEN = 'x-auth-token'
 export const PROJECT_NAME = 'envoix'
 export const CONFIG_FILE = 'env-config.json'
